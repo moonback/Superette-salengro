cfd918d4dcb76d041926099dac6093b92a2b6c14
Medusa <devoye.maysson@gmail.com>
2026-06-30 12:29:59 +0200

    u

--- dist-electron/main/index.js
+++ dist-electron/main/index.js
@@ -1,38 +1,27 @@
-import { app, BrowserWindow } from "electron";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
-process.env.DIST = path.join(__dirname$1, "../dist");
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, "../public");
-let win;
-const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
-function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+import { app as n, BrowserWindow as s } from "electron";
+import o from "path";
+import { fileURLToPath as a } from "url";
+const t = o.dirname(a(import.meta.url));
+process.env.DIST = o.join(t, "../dist");
+process.env.VITE_PUBLIC = n.isPackaged ? process.env.DIST : o.join(process.env.DIST, "../public");
+let e;
+const i = process.env.VITE_DEV_SERVER_URL;
+function r() {
+  e = new s({
+    icon: o.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname$1, "../preload/index.js")
+      preload: o.join(t, "../preload/index.js")
     }
-  });
-  win.webContents.on("did-finish-load", () => {
-    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  });
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(path.join(process.env.DIST, "index.html"));
-  }
+  }), e.webContents.on("did-finish-load", () => {
+    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  }), i ? e.loadURL(i) : e.loadFile(o.join(process.env.DIST, "index.html"));
 }
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    win = null;
-  }
+n.on("window-all-closed", () => {
+  process.platform !== "darwin" && (n.quit(), e = null);
 });
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+n.on("activate", () => {
+  s.getAllWindows().length === 0 && r();
 });
-app.whenReady().then(createWindow);
+n.whenReady().then(r);
